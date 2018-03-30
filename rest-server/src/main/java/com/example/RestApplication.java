4420595d5a582312ac25614d79d57189924e0f33
amarflybot <amar.arawat@gmail.com>
2018-03-30 13:43:40 +0530

    Change for UI Maven and Build

--- rest-server/src/main/java/com/example/RestApplication.java
+++ rest-server/src/main/java/com/example/RestApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestAppApplication.class, args);
+		SpringApplication.run(RestApplication.class, args);
 	}
 }
--- rest-server/src/test/java/com/example/RestApplicationTests.java
+++ rest-server/src/test/java/com/example/RestApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class RestAppApplicationTests {
+public class RestApplicationTests {
 
 	@Test
 	public void contextLoads() {
